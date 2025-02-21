48c2aecb2397fb72fae69fbb7b8280bd5b2f0143
Saeed Mafipour <m.saeed.mafipour@tum.de>
2025-02-21 10:23:36 +0100

    language switcher fixed

--- app/[locale]/not-found.tsx
+++ app/[locale]/not-found.tsx
@@ -1,7 +1,7 @@
 const NotFound = () => {
   return (
     <div className="h-screen w-screen flex items-center justify-center text-4xl font-bold">
-      not-found
+      Not Found!!
     </div>
   );
 };
