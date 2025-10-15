b3bcf37205b01a8af59ae1cee672b36f79aef405
Saeed Mafipour <m.saeed.mafipour@tum.de>
2025-10-15 17:20:00 +0200

    Updated

--- components/hero/ProfileImage.tsx
+++ components/hero/ProfileImage.tsx
@@ -3,7 +3,7 @@ import Image from "next/image";
 
 const ProfileImage = () => {
   return (
-    <div className="w-48 aspect-square relative bg-zinc-600 rounded-full overflow-hidden border-2 border-primary">
+    <div className="w-48 aspect-square relative bg-zinc-400 rounded-full overflow-hidden border-2 border-primary">
       <Image
         src={mediaFiles["profile"] as string}
         fill
