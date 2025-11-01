3ff0e79340eb4edeee86e3becb8318e5aeea9694
Saeed Mafipour <saeed.mafipour@gmail.com>
2025-11-01 10:43:35 +0100

    ui updated

--- components/providers.tsx
+++ components/providers.tsx
@@ -2,10 +2,12 @@
 import { ThemeProvider } from "@/components/theme-provider";
 import { TooltipProvider } from "./ui/tooltip";
 import { ModalProvider } from "@/context/CertificateModalContext";
+import { Analytics } from "@vercel/analytics/next";
 
 const Providers = ({ children }: { children: React.ReactNode }) => {
   return (
     <TooltipProvider>
+      <Analytics />
       <ModalProvider>
         <ThemeProvider
           attribute="class"
